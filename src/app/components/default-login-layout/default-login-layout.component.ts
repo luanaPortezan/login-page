369a212a62de1df6892fa0472ae8f906bf426a3a
Luana Portezan <luanaportezan@gmail.com>
2024-04-02 14:24:52 -0300

    feat: add default style

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class DefaultLoginLayoutComponent {
 
+  @Input()
+  title: string = '';
+  @Input()
+  primaryButtonText: string = '';
+  @Input()
+  secondaryButtonText: string = '';
 }
